b42e3f3cd62c0d0cfadba9c2100e0b0804c3205c
MutindaVictoria <victoriaamutinda@gmail.com>
2023-05-24 16:48:37 +0300

    Dom

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ document.getElementById('title').style.color='green'
 document.getElementById('heading').style.textTransform='UpperCase'
 document.getElementById('header').style.textTransform='UpperCase'
 
-document.getElementById('fruList').appendChild('strawberry')
+let li=document.createElement('li')
+li.innerHTML='strawberry';
+document.getElementById('fruList').appendChild(li);
+
+let li2=document.createElement('li2')
+li2.innerHTML='cabbages';
+document.getElementById('vegList').appendChild(li2)
+ 
