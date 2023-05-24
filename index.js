26272b420ebc9f1c649ad8f6106561c7e8bc0fa4
MutindaVictoria <victoriaamutinda@gmail.com>
2023-05-24 16:31:59 +0300

    dom

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById('background').style.backgroundColor='silver'
+
+document.getElementById('title').style.color='green'
+
+document.getElementById('heading').style.textTransform='UpperCase'
+document.getElementById('header').style.textTransform='UpperCase'
+
+document.getElementById('fruList').appendChild('strawberry')
